9fd4c5a89ce2b446baf9b9293338b15786c20645
lluis55 <jorge.cortes.lluis@gmail.com>
2025-06-09 10:50:39 -0600

    Add files via upload

---
+++ fibonacci 1.py
@@ -0,0 +1,15 @@
+X = 0
+Y = 1
+Nmaximo = 100
+while Y < Nmaximo:
+
+    
+    X, Y = Y, X + Y
+   # x = x + y
+   # y = x + y
+    
+    #if:
+     #   y >= 10
+      #  print(f"{y}")
+    #elif:
+print (f" El numero mas proximo a llegar o ser {Nmaximo} es: {X}")
