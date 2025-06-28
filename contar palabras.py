80dc9efb6948e9a6a4d18243617f0da3f0c966fc
lluis55 <jorge.cortes.lluis@gmail.com>
2025-06-28 13:38:47 -0600

    Add files via upload

--- contar palabras.py
+++ contar palabras.py
@@ -4,10 +4,25 @@ def contar (texto):
         raise ValueError ("no hay ninguina palabra para contar")
     return len(palabras)
 
-print("contador de palabras")
-texto = input("ingresa tu texto para contar las palabras que tiene: ")
-try:
-    numPalabras = contar(texto)
-    print("numero de palabras :", numPalabras)
-except ValueError:
-    print("ingresa al menos una palabra")
+
+
+print("contador de palabras y letras")
+
+while True:
+    texto = input("ingresa tu texto para contar las palabras que tiene: ")
+    try:
+        numPalabras = contar(texto)
+        contador = 0
+        for caracter in texto:
+            if caracter.isalpha():
+                contador += 1
+        print("numero de palabras :", numPalabras)
+        print("numero de letras: ", contador)
+
+
+    except ValueError:
+        print("ingresa al menos una palabra")
+    repetir = input("volver a ejecutar? (s) u oprime cualquier tecla para salir").strip().lower()
+    if repetir != "s":
+        print ("cerrando")
+        break
